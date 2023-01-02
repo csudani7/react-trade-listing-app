9c1185287054210a7b087a6e58da1f79b6d503c5
Chintan Sudani <csudani7@gmail.com>
2023-01-02 17:21:52 +0530

    feat: Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-800 text-white flex flex-col justify-center items-center h-screen space-y-40 p-16">
+      <div className="font-bold text-9xl">Hello World !</div>
+      <div className="font-medium text-9xl"> UI is Coming Soon....</div>
     </div>
   );
 }
